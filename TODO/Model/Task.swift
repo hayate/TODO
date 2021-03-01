4c14ece0a2501b42b342d84dd4fb5bd0864ccc8a
hayate <andrea@biv.io>
2021-03-02 08:01:42 +0900

    add global tasks saving

--- TODO/Model/Task.swift
+++ TODO/Model/Task.swift
@@ -18,25 +18,25 @@ class Task {
 
     private var tasks: [Todo]!
 
-    private init() {
+    private init() {}
+
+
+    public static func getInstance() -> Task {
+        return Instance
+    }
+
+    public func load() -> Void {
         if let data = UserDefaults.standard.data(forKey: "tasks") {
             self.tasks = self.fromJSON(data: data)
         } else {
             self.tasks = []
         }
-
-        /*
-        tasks = [
-            Todo(todo: "Watch byteset videos", done: true),
-            Todo(todo: "Make a Todo app", done: false),
-            Todo(todo: "Learn Table and Cell Views", done: false),
-            Todo(todo: "Build your own amazing app", done: false)
-        ]
-        */
     }
 
-    public static func getInstance() -> Task {
-        return Instance
+    public func save() -> Void {
+        if let data = self.toJSON() {
+            UserDefaults.standard.set(data, forKey: "tasks")
+        }
     }
 
     public func getTasks() -> [Todo] {
@@ -45,31 +45,19 @@ class Task {
 
     public func addTask(todo: Todo) -> Void {
         tasks.append(todo)
-        if let data = self.toJSON() {
-            UserDefaults.standard.set(data, forKey: "tasks")
-        }
     }
 
     public func addTask(todo: String) -> Void {
         tasks.append(Todo(todo: todo, done: false))
-        if let data = self.toJSON() {
-            UserDefaults.standard.set(data, forKey: "tasks")
-        }
     }
 
     public func removeTask(index: Int) -> Void {
         tasks.remove(at: index)
-        if let data = self.toJSON() {
-            UserDefaults.standard.set(data, forKey: "tasks")
-        }
     }
 
     public func removeTask(todo: Todo) -> Void {
         self.tasks = tasks.filter {item in
             return item != todo
-        }
-        if let data = self.toJSON() {
-            UserDefaults.standard.set(data, forKey: "tasks")
         }
     }
 
